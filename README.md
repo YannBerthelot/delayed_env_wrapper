fe532fb761d2d4c67892c3e76757d18cf66b2402
Yann Berthelot <yannberthelot1@gmail.com>
2023-07-29 15:33:08 +0200

    refactor gymnax wrapper to allow no changes for the user between wrapped and unwrapped, update readme accordingly

--- README.md
+++ README.md
@@ -20,13 +20,13 @@ import gymnax
 from delayed_env_wrapper.gymnasium_wrapper import ConstantDelayedWrapper
 
 base_env = gym.make("CartPole-v1")
-delayed_env = delayed_env = ConstantDelayedWrapper(base_env, delay=5)
+delayed_env = ConstantDelayedWrapper(base_env, delay=5)
 
 obs, info = delayed_env.reset()
 obs, reward, terminated, truncated, info = delayed_env.step(action)
 ```
 ### Gymnax
-You have to specify the delay and to propagate the action_buffer througought the processing.
+You just have to specify the delay.
 ```python
 import gymnasium as gym
 
@@ -35,8 +35,8 @@ from delayed_env_wrapper.gymnax_wrapper import ConstantDelayedWrapper
 base_env, env_params = gymnax.make("CartPole-v1")
 delayed_env = ConstantDelayedWrapper(base_env, delay=5)
 
-obs, env_state, action_buffer = delayed_env.reset(key_reset, env_params)
-n_obs, n_state, reward, done, info, action_buffer = delayed_env.step(key_step, env_state, action, action_buffer, env_params)
+obs, env_state = delayed_env.reset(key_reset, env_params)
+n_obs, n_state, reward, done, info = delayed_env.step(key_step, env_state, action, env_params)
 ```
 ### Initial actions
 Given that you have delay between the first timestep (for which you select an action) and when it affects the environment, actions need to be decided for the first d-timesteps (with d being the delay). To do so the environment will return the initial observation and reward until the first action had effect. Another possibility (NOT IMPLEMENTED YET) is to provide a serie of actions at the environment setup that will be executed while waiting for the first actual action to affect the environment.
--- tests/constant_delay/test_wrapper_on_gymnax.py
+++ tests/constant_delay/test_wrapper_on_gymnax.py
@@ -14,17 +14,17 @@ def setup_and_fill_buffer():
     base_env, env_params = gymnax.make("CartPole-v1")
     delay = 5
     delayed_env = ConstantDelayedWrapper(base_env, delay=delay)
-    _, env_state, action_buffer = delayed_env.reset(key_reset, env_params)
+    _, env_state  = delayed_env.reset(key_reset, env_params)
     initial_actions = [0, 1, 0, 1, 0]
     for (
         action
     ) in (
         initial_actions
     ):  # action buffer for actions before the first agent selected action
-        _, _, _, _, _, action_buffer = delayed_env.step(
-            key_step, env_state, action, action_buffer, env_params
+        _, env_state, _, _, _ = delayed_env.step(
+            key_step, env_state, action, env_params
         )
-    return delayed_env, initial_actions, env_params, env_state, key_step, action_buffer
+    return delayed_env, initial_actions, env_params, env_state, key_step
 
 
 def test_env_is_wrapper_of_base_env():
@@ -35,7 +35,8 @@ def test_env_is_wrapper_of_base_env():
 
 
 def test_fill_buffer_before_start(setup_and_fill_buffer):
-    delayed_env, initial_actions, _, _, _, action_buffer = setup_and_fill_buffer
+    delayed_env, initial_actions, _, env_state, _ = setup_and_fill_buffer
+    action_buffer = env_state.action_buffer
     assert jnp.array_equal(action_buffer, jnp.array(initial_actions))
 
 
@@ -46,12 +47,13 @@ def test_env_is_delayed(setup_and_fill_buffer):
         env_params,
         env_state,
         key_step,
-        action_buffer,
     ) = setup_and_fill_buffer
+    action_buffer = env_state.action_buffer
     actions = [1, 1, 0, 0, 1]
     for i, action in enumerate(actions):
-        action_buffer[0] == initial_actions[i]
-        action_buffer = delayed_env.step(
-            key_step, env_state, action, action_buffer, env_params
-        )[-1]
+        assert action_buffer[0] == initial_actions[i]
+        _, env_state, _, _, _ = delayed_env.step(
+            key_step, env_state, action, env_params
+        )
+        action_buffer = env_state.action_buffer
     assert jnp.array_equal(action_buffer, jnp.array(actions))
