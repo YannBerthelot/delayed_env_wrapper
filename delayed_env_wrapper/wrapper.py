895192ec6880b23fcb6215cbb96bde28e5c3bce4
Yann Berthelot <yannberthelot1@gmail.com>
2023-07-29 11:14:26 +0200

    initial commit with gymnasium compatibility

---
+++ delayed_env_wrapper/wrapper.py
@@ -0,0 +1,34 @@
+from typing import Any, SupportsFloat, TypeVar
+
+import gymnasium as gym
+from collections import deque
+
+ObsType = TypeVar("ObsType")
+ActType = TypeVar("ActType")
+WrapperObsType = TypeVar("WrapperObsType")
+WrapperActType = TypeVar("WrapperActType")
+
+class ConstantDelayedWrapper(gym.Wrapper):
+    def __init__(self, base_env, delay):
+        gym.Wrapper.__init__(self, base_env)
+        self._delay = delay
+        self._action_buffer = deque([None for _ in range(delay)], maxlen=delay)
+        self._action_to_exec = None
+    @property
+    def action_to_exec(self):
+        return self._action_buffer[0]
+
+    @property
+    def last_action_inserted(self):
+        return self._action_buffer[-1]
+    @property
+    def action_buffer(self):
+        return self._action_buffer
+    def step(
+            self, action: WrapperActType
+    ) -> tuple[WrapperObsType, SupportsFloat, bool, bool, dict[str, Any]]:
+        actual_action = self._action_buffer.popleft()
+        self._action_buffer.append(action)
+        if self.env._elapsed_steps >= self._delay:
+            return self.env.step(actual_action)
+
---
+++ tests/constant_delay/test_constant_delay_wrapper_on_gymnasium.py
@@ -0,0 +1,37 @@
+import gymnasium as gym
+import numpy as np
+import pytest
+from delayed_env_wrapper.wrapper import ConstantDelayedWrapper
+
+@pytest.fixture()
+def setup_and_fill_buffer():
+    base_env = gym.make("CartPole-v1")
+    delay = 5
+    delayed_env = ConstantDelayedWrapper(base_env, delay=delay)
+    delayed_env.reset()
+    initial_actions = [0, 1, 0, 1, 0]
+    for (
+            action
+    ) in (
+            initial_actions
+    ):  # action buffer for actions before the first agent selected action
+        delayed_env.step(action)
+    return delayed_env, initial_actions
+
+def test_env_is_wrapper_of_base_env():
+    base_env = gym.make("CartPole-v1")
+    delay = 5
+    delayed_env = ConstantDelayedWrapper(base_env, delay=delay)
+    assert base_env.unwrapped == delayed_env.unwrapped
+
+def test_fill_buffer_before_start(setup_and_fill_buffer):
+    delayed_env, initial_actions = setup_and_fill_buffer
+    assert np.array_equal(delayed_env.action_buffer, initial_actions)
+
+def test_env_is_delayed(setup_and_fill_buffer):
+    delayed_env, initial_actions = setup_and_fill_buffer
+    actions = [1, 1, 0, 0, 1]
+    for i, action in enumerate(actions):
+        assert delayed_env.action_to_exec == initial_actions[i]
+        delayed_env.step(action)
+    assert np.array_equal(delayed_env.action_buffer, actions)
